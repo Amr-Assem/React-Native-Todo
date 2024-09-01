656f7863ed3b3cc3ed94312c962abb20406363cb
Amr Assem <amr.assem@onmail.com>
2024-09-01 03:23:14 +0300

    Setup redux

--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -3,9 +3,21 @@ import { createSlice } from "@reduxjs/toolkit";
 const todoSlice = createSlice({
     name: "todoSlice",
     initialState: {
+        tasks: [],
     },
     reducers: {
+        addTask: (state, action) => {
+            const currentTasks = [...state.tasks]
+            const newTask = action.payload
+            state.tasks = [...currentTasks, newTask]
+        },
+        removeTask: (state, action) => {
+            const removedId = action.payload.id;
+            const filteredTasks = state.tasks.filter(task => task.id !== removedId);
+            state.tasks = filteredTasks
+        }
     }
 })
 
+export const { addTask, removeTask } = todoSlice.actions;
 export default todoSlice;
